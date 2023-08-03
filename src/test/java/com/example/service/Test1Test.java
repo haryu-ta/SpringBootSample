9a8c32fd9aa4e691fdcd1f4a967a79420dcea6dc
板村亮平 <ryoheiitamura@itamuraryouheinoMacBook-Air.local>
2023-08-03 20:13:55 +0900

    不要なimport削除

--- src/test/java/com/example/service/Test1Test.java
+++ src/test/java/com/example/service/Test1Test.java
@@ -3,23 +3,13 @@ package com.example.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 
-
-import static com.sun.javaws.JnlpxArgs.verify;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 
 @SpringBootTest(classes={Test1.class,Test2.class})
